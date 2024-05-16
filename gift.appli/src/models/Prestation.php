0519f2a4409b7288bd1466719ce30ff9e5555872
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-05-16 17:55:58 +0200

    modification prestation (changment namespace et nom fichier)

---
+++ gift.appli/src/models/Prestation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace gift\appli\src\models;
+
+class Prestation extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table="prestation";
+    protected $primaryKey="id";
+
+}
