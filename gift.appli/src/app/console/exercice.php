440872402f26f77bc573c82eaa24d8917fe22942
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-05-21 11:02:52 +0200

    Modification pour question 6

--- gift.appli/src/app/console/exercice.php
+++ gift.appli/src/app/console/exercice.php
@@ -9,6 +9,7 @@ use gift\appli\models\Prestation;
 use gift\appli\models\Categorie;
 use gift\appli\models\Box;
 use gift\appli\models\User;
+use Ramsey\Uuid\Uuid;
 
 use Illuminate\Database\Capsule\Manager as DB;
 
@@ -19,90 +20,120 @@ $db->addConnection($conf);
 $db->setAsGlobal();
 $db->bootEloquent();
 
-//Q-1
-echo "<br><br> ";
-echo "----------------- Q-1 -----------------";
-echo "<br><br>";
+//testConnection
 
+if($db->connection()->getDatabaseName()){
+    echo "Connected to the database \n";
+}
+
+
+//Q-1
+echo "\n\n";
+echo "----------------- Q-1 -----------------";
+echo "\n\n";
+
+$i = 0;
 $prestations = Prestation::all();
 foreach($prestations as $prestation){
-    $aff = $prestation->libelle . "<br>";
-    $aff.= $prestation->description . "<br>";
-    $aff.= $prestation->tarif . "<br>";
-    $aff.= $prestation->unite . "<br>";
+    $i++;
+    $aff = $i .' - '. $prestation->libelle . "\n";
+    $aff.= $prestation->description . "\n";
+    $aff.= $prestation->tarif . "\n";
+    $aff.= $prestation->unite . "\n";
     echo $aff;
 }
 
 //Q-2
-echo "<br><br> ";
+echo "\n\n\n";
 echo "----------------- Q-2 -----------------";
-echo "<br><br>";
+echo "\n\n\n";
 
+$i = 0;
 $prestations = Prestation::with('categorie')->get();
 foreach($prestations as $prestation){
-    $aff = $prestation->libelle . "<br>";
-    $aff.= $prestation->description . "<br>";
-    $aff.= $prestation->tarif . "<br>";
-    $aff.= $prestation->unite . "<br>";
-    $aff.= $prestation->categorie->libelle . "<br>";
+    $i++;
+    $aff =$i. ' - ' . $prestation->libelle . "\n";
+    $aff.= $prestation->description . "\n";
+    $aff.= $prestation->tarif . "\n";
+    $aff.= $prestation->unite . "\n";
+    $aff.= $prestation->categorie->libelle . "\n";
     echo $aff;
 }
 
 //Q-3
-echo "<br><br> ";
+echo "\n\n";
 echo "----------------- Q-3 -----------------";
-echo "<br><br>";
+echo "\n\n";
 
 $categorie = Categorie::find(3);
-$aff = $categorie->libelle . "<br>";
+$aff = $categorie->libelle . "\n";
 foreach($categorie->prestation as $prestation){
-    $aff.= $prestation->libelle . "<br>";
-    $aff.= $prestation->tarif . "<br>";
-    $aff.= $prestation->unite . "<br>";
+    $aff.= $prestation->libelle . "\n";
+    $aff.= $prestation->tarif . "\n";
+    $aff.= $prestation->unite . "\n";
 }
 echo $aff;
 
 //Q-4
-echo "<br><br> ";
+echo "\n\n";
 echo "----------------- Q-4 -----------------";
-echo "<br><br>";
+echo "\n\n";
 
 $box = Box::find('360bb4cc-e092-3f00-9eae-774053730cb2');
-$aff = $box->libelle . "<br>";
-$aff.= $box->description . "<br>";
-$aff.= $box->montant . "<br>";
+$aff = $box->libelle . "\n";
+$aff.= $box->description . "\n";
+$aff.= $box->montant . "\n";
 
 echo $aff;
 
 //Q-5
-echo "<br><br> ";
+echo "\n\n";
 echo "----------------- Q-5 -----------------";
-echo "<br><br>";
+echo "\n\n";
 
 $box = Box::find('360bb4cc-e092-3f00-9eae-774053730cb2');
-$aff = $box->libelle . "<br>";
-$aff.= $box->description . "<br>";
-$aff.= $box->montant . "<br>";
+$aff = $box->libelle . "\n";
+$aff.= $box->description . "\n";
+$aff.= $box->montant . "\n";
 
-foreach($box->prestation as $prestation){
-    $aff.= $prestation->libelle . "<br>";
-    $aff.= $prestation->tarif . "<br>";
-    $aff.= $prestation->unite . "<br>";
-    $aff.= $prestation->pivot->quantite . "<br>";
+foreach($box->box2presta as $prestation){
+    $aff.= $prestation->libelle . "\n";
+    $aff.= $prestation->tarif . "\n";
+    $aff.= $prestation->unite . "\n";
+    $aff.= $prestation->pivot->quantite . "\n";
 }
 
 echo $aff;
 
 //Q-6
 
-echo "<br><br> ";
+echo "\n\n ";
 echo "----------------- Q-6 -----------------";
-echo "<br><br>";
-/*
-$box = new Box();
-$box->id = Uuid::uuid4();
-$box->libelle = "Box 1";
-$box->description = "Description Box 1";
-$box->montant = 100;
-$box->save();
-*/
+echo "\n\n";
+
+$res = Box::where('libelle', 'Box 1')->first();
+
+if($res) {
+    echo "Box 1 existe \n";
+}else{
+    echo "Box 1 n'existe pas \n";
+    $box = new Box();
+    $box->id = Uuid::uuid4();
+    //pas pécisée dans le sujet donc je la génère aléatoirement
+    $box->token = Uuid::uuid4();
+    $box->libelle = "Box 1";
+    $box->description = "Description Box 1";
+    $box->montant = 100;
+    $box->save();
+
+    $prestation = Prestation::find(1);
+    $box->box2presta()->attach($prestation, ['quantite' => 2]);
+
+    $prestation = Prestation::find(2);
+    $box->box2presta()->attach($prestation, ['quantite' => 1]);
+
+    $prestation = Prestation::find(3);
+    $box->box2presta()->attach($prestation, ['quantite' => 3]);
+}
+
+
--- gift.appli/src/models/Box.php
+++ gift.appli/src/models/Box.php
@@ -2,10 +2,15 @@
 
 namespace gift\appli\models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
 class Box extends \Illuminate\Database\Eloquent\Model{
 
     protected $table="box";
     protected $primaryKey="id";
+
+    use HasUuids;
 
 
     public function box2presta(){
