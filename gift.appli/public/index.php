cc72e7448903219253b25c7039aab3488ae451d9
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-05-22 13:03:49 +0200

    début TD2 mise en place

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -2,8 +2,10 @@
 declare(strict_types=1);
 
 
+use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../src/vendor/autoload.php';
+
 
 /* application boostrap */
 
@@ -12,8 +14,9 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 
+$app->setBasePath('/archi-s4/gift.appli/public');
 
-$app=(require_once __DIR__ . '../src/conf/routes.php')($app);
+$app=(require_once __DIR__ . '/../src/conf/routes.php')($app);
 
 
 
--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -3,10 +3,15 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
 
 return function( \Slim\App $app): \Slim\App {
     /* home */
- 
+
+    $app->get('/', function (Request $request, Response $response, array $args) {
+        $response->getBody()->write("Hello, welcome to the gift-appli API");
+        return $response;
+    });
 
     return $app;
 
