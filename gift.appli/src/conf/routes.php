76f726ebf2daae08582fd3946bb57aba6d03e57c
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-05-22 15:15:01 +0200

    mise en commun Q3 et 4

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -10,10 +10,10 @@ return function( \Slim\App $app): \Slim\App {
 
     $app->get('/', function (Request $request, Response $response, array $args) {
         $response->getBody()->write("Hello, welcome to the gift-appli API");
-        return $response;
     });
 
-    /*exo3:*/
+
+    //exo3
     $app-> get('/prestation',function(Request $request ,Response $a){
         $id=$request->getQueryParams();
 
@@ -31,6 +31,41 @@ return function( \Slim\App $app): \Slim\App {
         {$aff = "ceci n'est pas un id valide";}
         $a->getBody()->write($aff);
         return $a;});
+
+
+    //exo4
+    $app->get('/box/create', function (Request $request, Response $response) {
+        $res = <<<HTML
+        <h1>Create a box</h1>
+        <form action="/box/create" method="post">
+            <label for="libelle">Libelle</label>
+            <input type="text" id="libelle" name="libelle">
+            <label for="description">Description</label>
+            <input type="text" id="description" name="description">
+            <label for="montant">Montant</label>
+            <input type="number" id="montant" name="montant"> 
+            <button  type="submit">Create</button>
+        </form>
+HTML;
+            $response->getBody()->write($res);
+            return $response;
+        });
+
+    $app->post('/box/create', function (Request $request, Response $response) {
+        $data = $request->getParsedBody();
+        $res = <<<HTML
+        <h1>Box created</h1>
+        <ul>
+            <li>Libelle : {$data['libelle']}</li>
+            <li>Description : {$data['description']}</li>
+            <li>Montant : {$data['montant']}</li>
+        </ul>
+HTML;
+
+            $response->getBody()->write($res);
+            return $response;
+        });
+
 
 
     return $app;
