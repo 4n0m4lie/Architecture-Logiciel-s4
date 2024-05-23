2b37c55902fbb2f36230b356f6ea124e2e898e34
clem-png <clement.netange6@etu.univ-lorraine.fr>
2024-05-23 11:32:02 +0200

    exo 2 TD3, correction bootstrap, mise en HTML Action et création action GetPrestaDeCategorie.php

---
+++ gift.appli/src/app/actions/GetPrestatDeCategorieAction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace gift\appli\app\actions;
+
+use gift\appli\models\Categorie;
+use gift\appli\utils\Eloquent;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class GetPrestatDeCategorieAction extends AbstractAction{
+
+    public function __invoke(Request $request, Response $response, array $args): Response {
+
+        Eloquent::init('../src/conf/gift.db.conf.ini.dist');
+
+        $categorie = Categorie::find($args['id']);
+        $aff = "<h2> ". $categorie->libelle ."</h2> ";
+        $i =0;
+        $aff.="<ul>";
+        foreach($categorie->prestation as $prestation){
+            $i++;
+            $aff.= "<li>";
+            $aff.= $i .' - '. $prestation->libelle;
+            $aff.= $prestation->tarif ;
+            $aff.= $prestation->unite ;
+            $aff.= "</li>";
+        }
+        $aff.="</ul>";
+
+        $res = <<<HTML
+{$aff}
+HTML;
+
+        $response->getBody()->write($res);
+        return $response;
+    }
+}
